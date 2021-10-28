731a89e68bc9034ce4d808024d1ff2b439cff9f2
nts6920 <nts6920@gmail.com>
2021-10-28 23:45:16 +0700

    feat dev: game_display

--- app/src/main/java/com/example/tictactoe/GameDisplay.java
+++ app/src/main/java/com/example/tictactoe/GameDisplay.java
@@ -3,6 +3,7 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class GameDisplay extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class GameDisplay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_display);
+    }
+    public void playAgainPress(View view){
+
+    }
+
+    public void homePress(View view){
+
     }
 }
--- app/src/main/java/com/example/tictactoe/GameLogic.java
+++ app/src/main/java/com/example/tictactoe/GameLogic.java
@@ -11,4 +11,5 @@ public class GameLogic extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_logic);
     }
+
 }
